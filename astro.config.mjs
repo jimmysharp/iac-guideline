2c02121de1d715400a8b6b1bb51d7e99cb7ca7d7
jimmysharp <jimmy.sharptech@gmail.com>
2024-08-18 23:11:38 +0900

    タイトル変更漏れ修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'Terraform Guideline',
+      title: 'IaC Guideline',
       locales: {
         root: {
           label: '日本語',
